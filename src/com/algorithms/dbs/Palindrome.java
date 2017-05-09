4dcd84f1712a303baa933f37d53392518052a75e
pragnani <pragnani@gmail.com>
2017-05-09 23:26:00 +0530

    Palindrome working

--- src/com/algorithms/dbs/Palindrome.java
+++ src/com/algorithms/dbs/Palindrome.java
@@ -5,14 +5,42 @@ import java.util.Scanner;
 public class Palindrome {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 		String inputString = sc.next();
 		sc.close();
-		if (inputString.length() > 0) {
-			char[] inputChars= inputString.toCharArray();
-			
+		int n = inputString.length();
+		int[][] values = new int[n][n];
+		int i, j;
 
+		for (i = 0; i < n; i++) {
+			values[i][i] = 1;
 		}
+		for(i = n - 2; i >= 0; i--) {
+	        for(j = i + 1; j < n; j++) {
+	 
+	            if(inputString.charAt(i) == inputString.charAt(j)) {
+	                values[i][j] = 2 + values[i + 1][j - 1];
+	            }
+	 
+	            else {
+	                values[i][j] = Math.max(values[i + 1][j], values[i][j - 1]);
+	            }
+	        }
+	    }
+	 
+	 
+		int m=0;
+	 
+		for(i=1;i<n;i++)
+		{
+			int temp=values[0][i-1]*values[i][n-1];
+			if(temp>m){
+				m=temp;
+	        }
+		}
+		System.out.println(m);
+
 
 	}
 
